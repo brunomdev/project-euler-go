e4b434e699f07bf6af336fc3bc7aad816335798b
Bruno Marcel <bruno.silva@oppa.com.br>
2017-07-14 16:56:51 -0300

    Renamed var test and change testpair to testData type

--- solutions/001_test.go
+++ solutions/001_test.go
@@ -2,18 +2,13 @@ package solutions
 
 import "testing"
 
-type testpair struct {
-	limit    int
-	expected int
-}
-
-var tests = []testpair{
+var testsSolution001 = []testData{
 	{10, 23},
 	{1000, 233168},
 }
 
 func TestSolution001(t *testing.T) {
-	for _, test := range tests {
+	for _, test := range testsSolution001 {
 		if sum := Solution001(test.limit); sum != test.expected {
 			t.Errorf("Solution001(%d) = %d, should be %d\n", test.limit, sum, test.expected)
 		}
