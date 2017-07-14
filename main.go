056bda6bad54a5cc2f1bef6b3e3c02a5f4d34cf9
Bruno Marcel <bruno.silva@oppa.com.br>
2017-07-14 16:58:28 -0300

    Solution 002 added to main.go

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ import s "github.com/brunomdev/project-euler-go/solutions"
 func main() {
 	fmt.Println("Project Euler")
 	fmt.Printf("Solution 001 = %d\n", s.Solution001(1000))
+	fmt.Printf("Solution 002 = %d\n", s.Solution002(4000000))
 }
