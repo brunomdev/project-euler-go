83a702b0cff1c649db014c6cfa789207f76adbb2
Bruno Marcel <bruno.m@outlook.com>
2017-07-17 12:50:24 -0300

    Answer to question 003 added to main.go

--- main.go
+++ main.go
@@ -7,4 +7,5 @@ func main() {
 	fmt.Println("Project Euler")
 	fmt.Printf("Solution 001 = %d\n", s.Solution001(1000))
 	fmt.Printf("Solution 002 = %d\n", s.Solution002(4000000))
+	fmt.Printf("Solution 003 = %d\n", s.Solution003(600851475143))
 }
